2b88fef17232b48c7f2728575e4efccf8bc9cbb3
Okechukwu O Uneze <unezeo@gmail.com>
2020-11-01 21:07:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  sv = 10000
+  lowest_key = nil
+  lowest_value = nil
   name_hash.each do |key, value|
     if value < sv
       sv = min
