a39d3ad3e628642a6f89cab1a47e219d958bb1d4
Okechukwu O Uneze <unezeo@gmail.com>
2020-11-01 21:08:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,10 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
   name_hash.each do |key, value|
-    if value < sv
-      sv = min
+    if lowest_value = nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
     end
   end
-  sv
+  lowest_value
 end
